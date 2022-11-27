8972580394c983cf7fbd78d93bafc6427cd9bf00
whwoohw <woojoo1114@naver.com>
2022-11-27 23:51:26 +0900

    edit recommend result page

--- src/pages/recommend/test.js
+++ src/pages/recommend/test.js
@@ -1,57 +1,19 @@
 import { Box, Typography } from "@mui/material";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
-import { GetRecommendedStudios } from "../../components/fetcher/fetcher";
+import { GetStudios } from "../../components/fetcher/fetcher";
 import Layout from "../../layouts/Layout";
 import { API_URL } from "../../config";
 import Link from "next/link";
 import StudioCarousel from "../../components/studios/StudioCarousel";
 import RecommendedPhotoList from "../../components/photos/RecommendedPhotoList";
 
-export default function Result() {
-  const router = useRouter();
-  const photoType = router.query.photoType;
-  const town = router.query.town;
-  const color = router.query.color;
-  const tag = router.query.tag;
+export default function Test() {
+  const studiosData = GetStudios();
 
-  const photoTypeList = photoType
-    ?.split('"')
-    .filter((element, index) => index % 2 !== 0);
-  const townList = town?.split('"').filter((element, index) => index % 2 !== 0);
-  const colorList = color
-    ?.split('"')
-    .filter((element, index) => index % 2 !== 0);
-  const tagList = tag?.split('"').filter((element, index) => index % 2 !== 0);
-
-  const formData = {
-    sex: router.query.sex,
-    age: router.query.age,
-  };
-
-  useEffect(async () => {
-    const res = await fetch(`${API_URL}/studio/recommend/`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      withCredentials: true,
-      body: JSON.stringify(formData),
-    });
-    const data = await res.json();
-    console.log(data);
-  }, []);
-
-  const recommendedStudiosData = GetRecommendedStudios(
-    photoTypeList,
-    townList,
-    colorList,
-    tagList
-  );
-
-  const studios = recommendedStudiosData.studios?.data;
-  const studiosLoading = recommendedStudiosData.studiosLoading;
-  const studiosError = recommendedStudiosData.studiosError;
+  const studios = studiosData.studios?.data;
+  const studiosLoading = studiosData.studiosLoading;
+  const studiosError = studiosData.studiosError;
 
   if (studiosLoading) return <div>Loading...</div>;
   if (studiosError) console.log(studiosError);
